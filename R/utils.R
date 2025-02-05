4dc552f4192ce01c54d73751fbffc78f4dc03b39
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-05 11:36:35 +0700

    Update yolo path

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  PATH2Model <<- system.file("YOLO", "yl11n-14-Jan25.pt", package = pkgname)
+  PATH2Model <<- paste0(system.file(package='MorphoTreat'),"/YOLO/yl11n-14-Jan25.pt")
 }
