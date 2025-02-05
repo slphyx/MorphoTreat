67cbd9eaaf4976bf373ccb30c122af40e3479202
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-05 11:16:17 +0700

    Update Description file and fix hard-code path

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,3 @@
-
-PATH2Model <- paste0(system.file(package='MorphoTreat'),"/YOLO/yl11n-14-Jan25.pt")
+.onLoad <- function(libname, pkgname) {
+  PATH2Model <<- system.file("YOLO", "yl11n-14-Jan25.pt", package = pkgname)
+}
