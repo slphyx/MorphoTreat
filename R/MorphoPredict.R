a1b762752abff94ec74cca8ce515c66b22e3c010
Thanakorn Waleejaroenpong <tanakorn_wal@cmu.ac.th>
2025-02-07 16:14:24 +0700

    Update document and fix bug result
    - fix bugs result are duplicated

--- R/MorphoPredict.R
+++ R/MorphoPredict.R
@@ -6,11 +6,25 @@
 #'  
 #' @param img.path Absoulte path to an input image file.
 #' @param yolo.opts Additional YOLO CLI options (optional).
+#' @param show.image Logical value indicating whether to display the predicted image. If set to FALSE,
+#'   the image will not be displayed. Default is TRUE.
+#'   
+#' @details This function calls the YOLO CLI via a system command and requires that the YOLO CLI is
+#' installed and accessible in the system path. It depends on a global variable \code{PATH2Model}, which is
+#' initialised in the package's \code{.onLoad} function (located in the util file) to point to the model file.
+#' The package also utilises the \code{magick} package for image reading and plotting.
+#' 
+#' @return A data frame containing detection results, with columns such as \code{class}, \code{x_center},
+#'   \code{y_center}, \code{width}, \code{height}, and \code{confidence}.
 #' 
 #' @export
 #' 
 #' @examples
-#' MorphoPredict(img.path = "d:/data/images/my_slides.jpg)
+#' # Example with image display (default):
+#' MorphoPredict(img.path = "inst/pics/malaria_sample.png")
+#' 
+#' # Example without image display:
+#' MorphoPredict(img.path = "inst/pics/malaria_sample.png", show.image = FALSE)
 #' 
 MorphoPredict <- function(img.path, yolo.opts=NULL, show.image=TRUE){
   message("==== MorphoTreat ==== \nby slphyx@outlook.com\n=====================\n")
@@ -18,6 +32,13 @@ MorphoPredict <- function(img.path, yolo.opts=NULL, show.image=TRUE){
   temp_output <- file.path(tempdir(), "yolo_output")
   result_folder <- file.path(temp_output, "result")
   label_folder <- file.path(result_folder, "labels")
+  
+  result_img <- file.path(result_folder, basename(img.path))
+  label_file <- file.path(label_folder, paste0(tools::file_path_sans_ext(basename(img.path)), ".txt"))
+  
+  if (file.exists(label_file)) {
+    unlink(label_file)
+  }
   
   dir.create(result_folder, showWarnings = FALSE, recursive = TRUE)
   
@@ -39,9 +60,6 @@ MorphoPredict <- function(img.path, yolo.opts=NULL, show.image=TRUE){
   message("running command: ",cmd)
   
   system(cmd)
-  
-  result_img <- file.path(result_folder, basename(img.path))
-  label_file <- file.path(label_folder, paste0(tools::file_path_sans_ext(basename(img.path)), ".txt"))
   
   if (!file.exists(result_img)) {
     stop("Error: YOLO did not generate output image.")
